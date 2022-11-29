b52ed43b4957707d9f7090eb4cffab01eb4ade35
Miguel B. Henriques <103072223+MiguelHenri@users.noreply.github.com>
2022-11-29 14:54:26 -0300

    Create pcv.c

---
+++ pcv/pcv.c
@@ -0,0 +1,14 @@
+//contributor: https://github.com/zLeonardoIshida
+
+/*SCC0202 - Algoritmos e Estruturas de Dados I Prof. Marcelo Manzato
+* 
+*Alunos: 
+*   Miguel Bragante Henriques, NUSP: 13671894
+*   Leonardo Ishida, NUSP: 12873424
+*/
+
+#include "pcv.h"
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
